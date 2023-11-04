e33e66ceced92a7f9fe09267153f385d8cc6f800
s2curitydev@gmail.com <s2curitydev@gmail.com>
2023-11-03 22:13:04 -0700

    Record high score to file

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -23,6 +24,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
